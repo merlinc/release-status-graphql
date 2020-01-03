71beb9bf937c40407e46de981c311c957c6d45c7
Robin Harrison <merlinc@courts.io>
2020-01-03 01:41:29 +0000

    Fix comment

--- src/__tests__/utils.js
+++ src/__tests__/utils.js
@@ -1,20 +1,7 @@
+// This is a copy of the root index.js
+// FIXME: Refactor
+
 const fs = require('fs');
-
-// bootstrap config
-// load config
-// load projects config
-
-/*
-In a nutshell: define only external source connection settings in files,
-using those settings to connect to the source. Once connected, read
-additional configurations and add them to the config object returned
-from require('config');.
-
-Make sure any external overrides are done in the application bootstrap -
-before anyone calls the first config.get();, because the config object is
-made immutable as soon as any client uses the values via get().
-
-*/
 
 const { ApolloServer, gql } = require('apollo-server');
 
